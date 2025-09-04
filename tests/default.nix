fa932e11e8def50e471c6067985c4f96ca493af9
Thomas Prescher <thomas.prescher@cyberus-technology.de>
2025-09-04 22:10:31 +0200

    xxx

--- tests/default.nix
+++ tests/default.nix
@@ -3,119 +3,15 @@
   libvirt-src,
   nixos-image,
 }:
-let
-  common = import ./common.nix { inherit libvirt-src nixos-image; };
-in
-pkgs.nixosTest {
-  name = "Libvirt test";
 
-  extraPythonPackages = p: with p; [ pytest libvirt ];
+{
+  default = import ./libvirt-test.nix {
+    inherit pkgs libvirt-src nixos-image;
+    testScriptFile = ./testscript.py;
+  };
 
-  nodes.controllerVM =
-    { ... }:
-    {
-      imports = [
-        common
-        ../modules/nfs-host.nix
-      ];
-
-      virtualisation = {
-        cores = 4;
-        memorySize = 4096;
-        interfaces = {
-          eth1 = {
-            vlan = 1;
-          };
-        };
-        # controller hosts the NFS and needs more storage than the compute
-        # node.
-        diskSize = 8192;
-        forwardPorts = [
-          # Make the VM reachable from the Host or the test script via port 2222
-          { from = "host"; host.port = 2222; guest.port = 22; }
-          # Make Libvirt's TCP socket reachable from Host or the test script
-          { from = "host"; host.port = 2223; guest.port = 16509; }
-        ];
-      };
-
-      networking.extraHosts = ''
-        192.168.100.2 computeVM computeVM.local
-      '';
-
-      systemd.network = {
-        enable = true;
-        wait-online.enable = false;
-
-        networks = {
-          eth0 = {
-            matchConfig.Name = [ "eth0" ];
-            networkConfig = {
-              DHCP = "yes";
-            };
-          };
-          eth1 = {
-            matchConfig.Name = [ "eth1" ];
-            networkConfig = {
-              Address = "192.168.100.1/24";
-              Gateway = "192.168.100.1";
-              DNS = "8.8.8.8";
-            };
-          };
-        };
-      };
-    };
-
-  nodes.computeVM =
-    { ... }:
-    {
-      imports = [
-        common
-        ../modules/nfs-client.nix
-      ];
-
-      networking.extraHosts = ''
-        192.168.100.1 controllerVM controllerVM.local
-      '';
-
-      virtualisation = {
-        cores = 4;
-        memorySize = 4096;
-        interfaces = {
-          eth1 = {
-            vlan = 1;
-          };
-        };
-        diskSize = 2048;
-        # Make the VM reachable from the Host or the test script via port 3333
-        forwardPorts = [
-          { from = "host"; host.port = 3333; guest.port = 22; }
-        ];
-      };
-
-      livemig.nfs.host = "192.168.100.1";
-
-      systemd.network = {
-        enable = true;
-        wait-online.enable = false;
-
-        networks = {
-          eth0 = {
-            matchConfig.Name = [ "eth0" ];
-            networkConfig = {
-              DHCP = "yes";
-            };
-          };
-          eth1 = {
-            matchConfig.Name = [ "eth1" ];
-            networkConfig = {
-              Address = "192.168.100.2/24";
-              Gateway = "192.168.100.1";
-              DNS = "8.8.8.8";
-            };
-          };
-        };
-      };
-    };
-
-  testScript = { ... }: builtins.readFile ./testscript.py;
+  long = import ./libvirt-test.nix {
+    inherit pkgs libvirt-src nixos-image;
+    testScriptFile = ./testscript_long.py;
+  };
 }
---
+++ tests/libvirt-test.nix
@@ -0,0 +1,107 @@
+{
+  pkgs,
+  libvirt-src,
+  nixos-image,
+  testScriptFile,
+}:
+let
+  common = import ./common.nix { inherit libvirt-src nixos-image; };
+in
+pkgs.nixosTest {
+  name = "Libvirt test";
+
+  extraPythonPackages = p: with p; [ pytest libvirt ];
+
+  nodes.controllerVM = { ... }: {
+    imports = [
+      common
+      ../modules/nfs-host.nix
+    ];
+
+    virtualisation = {
+      cores = 4;
+      memorySize = 4096;
+      interfaces.eth1.vlan = 1;
+      diskSize = 8192;
+      forwardPorts = [
+        { from = "host"; host.port = 2222; guest.port = 22; }
+        { from = "host"; host.port = 2223; guest.port = 16509; }
+      ];
+      qemu.options = [
+        "-netdev tap,id=tap2,ifname=tap2,script=no,downscript=no"
+        "-device virtio-net-pci,netdev=tap2,mac=52:54:00:12:01:11,mq=on,vectors=9"
+      ];
+    };
+
+    networking.extraHosts = ''
+      192.168.111.2 computeVM computeVM.local
+    '';
+
+    systemd.network = {
+      enable = true;
+      wait-online.enable = false;
+      networks = {
+        eth0 = {
+          matchConfig.Name = [ "eth0" ];
+          networkConfig.DHCP = "yes";
+        };
+        eth1 = {
+          matchConfig.Name = [ "eth1" ];
+          networkConfig = {
+            Address = "192.168.100.1/24";
+            Gateway = "192.168.100.1";
+            DNS = "8.8.8.8";
+          };
+        };
+      };
+    };
+  };
+
+  nodes.computeVM = { ... }: {
+    imports = [
+      common
+      ../modules/nfs-client.nix
+    ];
+
+    networking.extraHosts = ''
+      192.168.111.1 controllerVM controllerVM.local
+    '';
+
+    virtualisation = {
+      cores = 4;
+      memorySize = 4096;
+      interfaces.eth1.vlan = 1;
+      diskSize = 2048;
+      forwardPorts = [
+        { from = "host"; host.port = 3333; guest.port = 22; }
+      ];
+      qemu.options = [
+        "-netdev tap,id=tap3,ifname=tap3,script=no,downscript=no"
+        "-device virtio-net-pci,netdev=tap3,mac=52:54:00:12:01:22,mq=on,vectors=9"
+      ];
+    };
+
+    livemig.nfs.host = "192.168.100.1";
+
+    systemd.network = {
+      enable = true;
+      wait-online.enable = false;
+      networks = {
+        eth0 = {
+          matchConfig.Name = [ "eth0" ];
+          networkConfig.DHCP = "yes";
+        };
+        eth1 = {
+          matchConfig.Name = [ "eth1" ];
+          networkConfig = {
+            Address = "192.168.100.2/24";
+            Gateway = "192.168.100.1";
+            DNS = "8.8.8.8";
+          };
+        };
+      };
+    };
+  };
+
+  testScript = { ... }: builtins.readFile testScriptFile;
+}
