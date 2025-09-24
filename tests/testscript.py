4a0389f0a8f52c2026cfc677c612655b1e32a0d0
Thomas Prescher <thomas.prescher@cyberus-technology.de>
2025-09-24 15:07:07 +0200

    XXX: endless boot test

--- tests/testscript.py
+++ tests/testscript.py
@@ -276,6 +276,20 @@ class LibvirtTests(unittest.TestCase):
 
         assert number_of_devices(controllerVM) == num_devices_old
 
+    def test_boot_loop(self):
+        count = 1
+        while True:
+            print("\n\n\n-------- Running loop {} --------\n\n\n".format(count))
+            count = count + 1
+            controllerVM.succeed("virsh define /etc/domain-chv.xml")
+            controllerVM.succeed("virsh start testvm")
+
+            if wait_for_ssh(controllerVM) == False:
+                print("XXXXX: ERROR connecting to VM")
+                breakpoint()
+
+            controllerVM.succeed("virsh shutdown testvm")
+
     def test_libvirt_restart(self):
         """
         We test the restart of the libvirt daemon. A restart requires that
@@ -800,27 +814,28 @@ class LibvirtTests(unittest.TestCase):
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(LibvirtTests("test_hotplug"))
-    suite.addTest(LibvirtTests("test_libvirt_restart"))
-    suite.addTest(LibvirtTests("test_live_migration"))
-    suite.addTest(LibvirtTests("test_live_migration_with_hotplug"))
-    suite.addTest(LibvirtTests("test_live_migration_with_hugepages"))
-    suite.addTest(LibvirtTests("test_live_migration_with_hugepages_failure_case"))
-    suite.addTest(LibvirtTests("test_live_migration_with_hotplug_and_virtchd_restart"))
-    suite.addTest(LibvirtTests("test_numa_topology"))
-    suite.addTest(LibvirtTests("test_hugepages"))
-    suite.addTest(LibvirtTests("test_hugepages_prefault"))
-    suite.addTest(LibvirtTests("test_numa_hugepages"))
-    suite.addTest(LibvirtTests("test_numa_hugepages_prefault"))
-    suite.addTest(LibvirtTests("test_network_hotplug_attach_detach_transient"))
-    suite.addTest(LibvirtTests("test_network_hotplug_attach_detach_persistent"))
-    suite.addTest(LibvirtTests("test_network_hotplug_transient_vm_restart"))
-    suite.addTest(LibvirtTests("test_network_hotplug_persistent_vm_restart"))
-    suite.addTest(LibvirtTests("test_network_hotplug_persistent_transient_detach_vm_restart"))
-    suite.addTest(LibvirtTests("test_serial_file_output"))
-    suite.addTest(LibvirtTests("test_managedsave"))
-    suite.addTest(LibvirtTests("test_shutdown"))
-    suite.addTest(LibvirtTests("test_libvirt_event_stop_failed"))
+    # suite.addTest(LibvirtTests("test_hotplug"))
+    suite.addTest(LibvirtTests("test_boot_loop"))
+    # suite.addTest(LibvirtTests("test_libvirt_restart"))
+    # suite.addTest(LibvirtTests("test_live_migration"))
+    # suite.addTest(LibvirtTests("test_live_migration_with_hotplug"))
+    # suite.addTest(LibvirtTests("test_live_migration_with_hugepages"))
+    # suite.addTest(LibvirtTests("test_live_migration_with_hugepages_failure_case"))
+    # suite.addTest(LibvirtTests("test_live_migration_with_hotplug_and_virtchd_restart"))
+    # suite.addTest(LibvirtTests("test_numa_topology"))
+    # suite.addTest(LibvirtTests("test_hugepages"))
+    # suite.addTest(LibvirtTests("test_hugepages_prefault"))
+    # suite.addTest(LibvirtTests("test_numa_hugepages"))
+    # suite.addTest(LibvirtTests("test_numa_hugepages_prefault"))
+    # suite.addTest(LibvirtTests("test_network_hotplug_attach_detach_transient"))
+    # suite.addTest(LibvirtTests("test_network_hotplug_attach_detach_persistent"))
+    # suite.addTest(LibvirtTests("test_network_hotplug_transient_vm_restart"))
+    # suite.addTest(LibvirtTests("test_network_hotplug_persistent_vm_restart"))
+    # suite.addTest(LibvirtTests("test_network_hotplug_persistent_transient_detach_vm_restart"))
+    # suite.addTest(LibvirtTests("test_serial_file_output"))
+    # suite.addTest(LibvirtTests("test_managedsave"))
+    # suite.addTest(LibvirtTests("test_shutdown"))
+    # suite.addTest(LibvirtTests("test_libvirt_event_stop_failed"))
     return suite
 
 def wait_until_succeed(func):
